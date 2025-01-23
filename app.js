b9ee69acb5addae0f7a42a5374f0199c4d984200
somi11 <somisun41@gmail.com>
2025-01-23 21:07:46 +0500

    mini app in vanilajs

---
+++ app.js
@@ -0,0 +1,10 @@
+import Store from "./services/Store.js";
+import { loadData } from "./services/Menu.js";
+
+window.app = {}
+app.store = Store
+
+window.addEventListener("DOMContentLoaded" , async () => {
+loadData();
+
+})
