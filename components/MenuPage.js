1a6288ae5d42bac5e78a0cae177352cafd1b4a6f
null <null>
2025-01-26 10:12:23 +0500

    working on web components

---
+++ components/MenuPage.js
@@ -0,0 +1,26 @@
+export class MenuPage extends HTMLElement {
+    constructor() {
+        super();
+     this.root = this.attachShadow({mode : 'open'})
+     
+     const styles = document.createElement("style");
+     this.root.appendChild(styles);
+      
+     async function loadCSS() {
+        const request = await fetch("/components/MenuPage.css");
+        const css = await request.text();
+        styles.textContent = css;
+     }
+
+     loadCSS();
+    }
+    //when comp is attached to dom
+    connectedCallback() {
+        
+        const template = document.getElementById("menu-page-template");
+        const content = template.content.cloneNode(true);
+        this.root.appendChild(content); 
+
+    }
+}
+customElements.define("menu-page" , MenuPage);
